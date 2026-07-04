3b65c0edc8f2bc26a370411e8c6cd9cdd19e6ef1
roozman <roozbeh.hmn@proton.me>
2026-07-04 10:17:21 +0330

    added userservice for registration logic - added a utility package for phone number validation

---
+++ go.mod
@@ -0,0 +1,3 @@
+module quizGameGo
+
+go 1.25.4
---
+++ service/userService/service.go
@@ -0,0 +1,64 @@
+package userService
+
+import (
+	"fmt"
+	"quizGameGo/entities"
+	"quizGameGo/pkg/phoneNumber"
+)
+
+type Reposityory interface {
+	IsPhoneNumberUnique(phoneNumber string) (bool, error)
+	Register(u entities.User) (entities.User, error)
+}
+
+type service struct {
+	repo Reposityory
+}
+
+type RegisterRequest struct {
+	Name        string
+	PhoneNumber string
+}
+
+type RegisterResponse struct {
+	User entities.User
+}
+
+func (s service) Register(req RegisterRequest) (RegisterResponse, error) {
+	// TODO - phone number should be verified via sms
+	// validate phone number
+	if !phoneNumber.IsValid(req.PhoneNumber) {
+		return RegisterResponse{}, fmt.Errorf("invalid phone number")
+	}
+
+	// checking phone number uniqueness
+	if isUnique, err := s.repo.IsPhoneNumberUnique(req.PhoneNumber); err != nil || !isUnique {
+		if err != nil {
+			return RegisterResponse{}, fmt.Errorf("unexpected error %w", err)
+		}
+		if !isUnique {
+			return RegisterResponse{}, fmt.Errorf("phone number is not unique")
+		}
+	}
+
+	// validate name
+	if len(req.Name) < 3 {
+		return RegisterResponse{}, fmt.Errorf("name is too short")
+	}
+
+	// create new user
+	user := entities.User{
+		ID:          0, //db handles id management
+		PhoneNumber: req.PhoneNumber,
+		Name:        req.Name,
+	}
+	createdUser, err := s.repo.Register(user)
+	if err != nil {
+		return RegisterResponse{}, fmt.Errorf("unexpected error %w", err)
+	}
+
+	// return created user
+	return RegisterResponse{
+		User: createdUser,
+	}, nil
+}
