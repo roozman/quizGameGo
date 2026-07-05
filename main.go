ddec31434ae7d77577c9b2f6e14a77436de8a863
roozman <roozbeh.hmn@proton.me>
2026-07-05 11:39:31 +0330

    implemented the isphonenumberunique methode

--- main.go
+++ main.go
@@ -7,12 +7,16 @@ import (
 )
 
 func main() {
+	testUserCreationRepo()
+}
+
+func testUserCreationRepo() {
 	// TODO - failed user registration increments the id - fix it
 	mysqlRepo := mysql.New()
 
 	createdUser, err := mysqlRepo.Register(entities.User{
 		ID:          0,
-		PhoneNumber: "0933",
+		PhoneNumber: "09323",
 		Name:        "hiu",
 	})
 
@@ -21,4 +25,10 @@ func main() {
 	} else {
 		fmt.Println("created user:", createdUser)
 	}
+
+	isUnique, err := mysqlRepo.IsPhoneNumberUnique(createdUser.PhoneNumber)
+	if err != nil {
+		fmt.Println("unique user phone number error:", err)
+	}
+	fmt.Println("isUnique:", isUnique)
 }
--- repository/mysql/user.go
+++ repository/mysql/user.go
@@ -1,18 +1,31 @@
 package mysql
 
 import (
+	"database/sql"
 	"fmt"
 	"quizGameGo/entities"
 )
 
 func (d *MySQLDB) IsPhoneNumberUnique(phoneNumber string) (bool, error) {
-	panic("implement me")
+	user := entities.User{}
+	var createdAt []uint8
+
+	row := d.db.QueryRow(`SELECT * from users where phone_number = ?`, phoneNumber)
+	err := row.Scan(&user.ID, &user.Name, &user.PhoneNumber, &createdAt)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return true, nil
+		}
+		return false, fmt.Errorf("cant scan query result: %w", err)
+	}
+	return false, nil
 }
 
 func (d *MySQLDB) Register(u entities.User) (entities.User, error) {
 	result, err := d.db.Exec(`INSERT INTO users(name, phone_number) values(?, ?)`, u.Name, u.PhoneNumber)
 	if err != nil {
-		return entities.User{}, fmt.Errorf("Error inserting user: %w", err)
+		return entities.User{}, fmt.Errorf("error inserting user: %w", err)
 	}
 
 	id, _ := result.LastInsertId()
